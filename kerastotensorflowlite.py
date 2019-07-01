4e01b90efb64ba909a6bd63224ad4504b6ac61a1
Armando Herrera <anhydrous99@gmail.com>
2019-07-01 14:01:21 -0500

    Newer quantized model

--- kerastotensorflowlite.py
+++ kerastotensorflowlite.py
@@ -3,7 +3,6 @@ import argparse
 import numpy as np
 import tensorflow as tf
 from freeze import freeze_session
-from data_parse import mean, std
 from tensorflow.python.keras.models import model_from_json
 from tensorflow.python.keras import backend as K
 K.set_learning_phase(0)
@@ -26,11 +25,6 @@ parser.add_argument(
     required=True,
     help='Model Name'
 )
-parser.add_argument(
-    '-d', '--data',
-    required=True,
-    help='Data path'
-)
 args = parser.parse_args()
 
 model = model_from_json(open(args.json_keras).read(), custom_objects={'tf': tf, 'RESIZE_FACTOR': 2})
@@ -39,21 +33,20 @@ model.load_weights(args.keras)
 input_arrays = ['input_image']
 
 output_names = [out.op.name for out in model.outputs]
+new_output_names = ['pred_score_map/Tanh', 'rbox_geo_map/Tanh', 'rbox_angle_map/Tanh']
 print(output_names)
 
-frozen_graph = freeze_session(K.get_session(), output_names=[out.op.name for out in model.outputs])
+frozen_graph = freeze_session(K.get_session(), output_names=output_names)
 
 
 tf.io.write_graph(frozen_graph, args.name, 'frozen_inference_graph.pb', as_text=False)
 tf_model_path = os.path.join(args.name, 'frozen_inference_graph.pb')
 
-
-converter = tf.lite.TFLiteConverter.from_frozen_graph(tf_model_path, input_arrays, output_names,
+converter = tf.lite.TFLiteConverter.from_frozen_graph(tf_model_path, input_arrays, new_output_names,
                                                       input_shapes={'input_image': [1, 512, 512, 3]})
 converter.inference_type = tf.uint8
-
-converter.quantized_input_stats = {'input_image': (10, 127)}
-converter.default_ranges_stats = (0, 1)
+converter.quantized_input_stats = {'input_image': (128, 127)}
+converter.default_ranges_stats = (0, 6)
 
 tflite_model = converter.convert()
 tflite_model_path = os.path.join(args.name, args.name + '.tflite')
@@ -70,11 +63,16 @@ output_details = interpreter.get_output_details()
 
 # Test model on random input data.
 input_shape = input_details[0]['shape']
-input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+input_data = np.array(np.random.random_sample(input_shape), dtype=np.uint8)
 interpreter.set_tensor(input_details[0]['index'], input_data)
 
 interpreter.invoke()
 output_data1 = interpreter.get_tensor(output_details[0]['index'])
 output_data2 = interpreter.get_tensor(output_details[1]['index'])
+output_data3 = interpreter.get_tensor(output_details[2]['index'])
 print(output_data1.shape)
 print(output_data2.shape)
+print(output_data3.shape)
+print(output_data1)
+print(output_data2)
+print(output_data3)
